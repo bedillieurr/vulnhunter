445ad8331f52367ef5ec28f4b32f6a332add33c4
Bedil Lieurr <154463051+bedillieurr@users.noreply.github.com>
2025-04-18 10:00:01 +0700

    main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import argparse
+from core.portscanner import scan_target
+
+def print_results(results):
+    print("\n[+] Scan Results:")
+    for res in results:
+        print(f"  - Port {res['port']} [{res['state']}]: {res['product']} {res['version']} ({res['name']})")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="VulnHunt - Port Scanner")
+    parser.add_argument("target", help="IP address or domain to scan")
+    parser.add_argument("--ports", help="Ports to scan (default: 1-1000)", default="1-1000")
+    args = parser.parse_args()
+
+    results = scan_target(args.target, args.ports)
+    print_results(results)
